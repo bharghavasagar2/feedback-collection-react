96f122710c3a15e154134ac26dc16de266fe8972
bharghav sagar <sagarbharghav2@gmail.com>
2019-11-02 23:57:34 +0530

    changed there to modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hello: 'world' });
+  res.send({ hello: 'modified' });
 });
 
 const PORT = process.env.PORT || 5000;
