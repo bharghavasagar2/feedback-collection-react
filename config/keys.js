62a439811c8b2e67c9f420194c0fc70d1463b0c0
bharghav sagar <sagarbharghav2@gmail.com>
2019-11-04 07:47:48 +0530

    arfa

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,11 @@
-//what setof u provide
+// keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
-  //we are in prod
+  // we are in production - return the prod set of keys
   module.exports = require('./prod');
 } else {
-  // we in development
+  // we are in development - return the dev keys!!!
   module.exports = require('./dev');
 }
-
 // emaily-prod
 // 1063365863772-9rqh3gqusk5gpf0koj5pcepoji2mb2tm.apps.googleusercontent.com--googleClientID
 // myKf_fiFGsoBysugr-af4xWk-this.googleClientSecret
